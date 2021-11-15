d6afaa46c39d946d9fad5a4b152148860ed349aa
Tulir Asokan <tulir@maunium.net>
2021-11-16 00:54:23 +0200

    Move filter_matrix_event logic to mautrix-python

--- mautrix_instagram/portal.py
+++ mautrix_instagram/portal.py
@@ -191,11 +191,7 @@ class Portal(DBPortal, BasePortal):
 
     async def _handle_matrix_message(self, sender: 'u.User', message: MessageEventContent,
                                      event_id: EventID) -> None:
-        if ((message.get(self.bridge.real_user_content_key, False)
-             and await p.Puppet.get_by_custom_mxid(sender.mxid))):
-            self.log.debug(f"Ignoring puppet-sent message by confirmed puppet user {sender.mxid}")
-            return
-        elif not sender.is_connected:
+        if not sender.is_connected:
             await self._send_bridge_error(
                 sender,
                 Exception("You're not connected to Instagram"),
