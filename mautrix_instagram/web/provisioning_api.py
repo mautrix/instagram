6e31ec7e8ac6058eccd3b6e8c08fa20d9bea6721
Nick Barrett <nick@beeper.com>
2023-02-13 16:07:37 +0000

    Fix formatting

--- mautrix_instagram/web/provisioning_api.py
+++ mautrix_instagram/web/provisioning_api.py
@@ -783,7 +783,9 @@ class ProvisioningAPI:
         except IGCheckpointError as e:
             return self._checkpoint_error(user, "<facebook credentials>", e, after="facebook auth")
         except IGChallengeError as e:
-            self.log.debug("%s logged in with facebook credentials, but got a challenge", user.mxid)
+            self.log.debug(
+                "%s logged in with facebook credentials, but got a challenge", user.mxid
+            )
             return await self.start_checkpoint(user, state, api, e, after="facebook auth")
         except IGConsentRequiredError as e:
             return self._consent_error(user, "<facebook credentials>", e, after="facebook auth")
